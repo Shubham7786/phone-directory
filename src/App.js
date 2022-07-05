4952e15f3038ceb123cc92ca1cfded2585e94d19
Shubham7786 <shubham00241@gmail.com>
2022-07-05 17:06:48 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import React from 'react';
 
 function App() {
   return (
-    <div>
-      Phone Directory
-    </div>
+    <React.Fragment>
+      {/* <div className='header'>
+        <div>
+          Phone Directory
+        </div>
+        <button> Add </button>
+        <div>
+          <span> Name </span><br />
+          <span> Phone </span>
+        </div>
+      </div> */}
+      <label htmlFor="name"> Name: </label>
+      <input id="name" type="text" placeholder="Type here" defaultValue="Shubh"/>
+    </React.Fragment>
   );
 }
 

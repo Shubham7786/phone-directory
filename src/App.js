458d12f7ad2e2b252c75d170aa8e60ff42b47590
Shubham7786 <shubham00241@gmail.com>
2022-07-06 17:55:31 +0530

    Passed heading to Header component as props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import Header from './Header'
+import "./App.css";
 
 function App() {
+  let subscribers = [
+    {
+      id: 1,
+      name: "Shubh",
+      phone: "7777777777"
+    },
+    {
+      id: 2,
+      name: "Ranjit",
+      phone: "9999999999"
+    }
+  ];
   return (
     <React.Fragment>
-        <Header/>
-       
-        <button> Add </button>
-        <div>
-          <span> Name </span><br />
-          <span> Phone </span>
+        <Header heading="Phone Directory"/>
+       <div className="component-body-cintainer">
+        <button className="custom-btn add-btn"> Add </button>
+        <div className="grid-container heading-container">
+          <span className="grid-item name-heading"> Name </span><br />
+          <span className="grid-item phone-heading"> Phone </span>
         </div>
-      
-      
+        {
+            subscribers.map(sub => {
+              return <div key={sub.id} className="grid-container">
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+              </div>
+            })
+          }
+
+
+        </div>
     </React.Fragment>
   );
 }
